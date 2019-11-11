1cf24f1940be4690ef9e7e13d78b97c97b977bab
Matt Crowley <mattccrowley@gmail.com>
2019-11-10 19:28:33 -0800

    Defined initial interfaces/classes and installed react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface Product {
+  sku: string;
+  unitPrice: number;
+  name: string;
+  quantity: number;
+}
+
+interface Cart {
+  selectedItems: Product[];
+}
+
+class ReviewCart extends Component {
+
+}
+
+class BillingShipping extends Component {
+
+}
+
+class OrderComplete extends Component {
+
+}
+
+class App extends Component<{}, Cart> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      selectedItems: []
+    }
+  }
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
 }
 
 export default App;
